f19408ca64ea21168f4a9a0a165b0f9fd765cdaf
Lbeenc <Lbeenc@gmail.com>
2025-03-18 10:54:46 -0500

    Update worker.c

--- worker.c
+++ worker.c
@@ -1,12 +1,9 @@
-/* worker.c - Child Process */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <sys/shm.h>
-#include <string.h>
+#include <sys/types.h>
 
 struct msgbuf {
     long mtype;
@@ -15,39 +12,35 @@ struct msgbuf {
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: worker <seconds> <nanoseconds>\n");
+        fprintf(stderr, "Usage: %s <seconds> <nanoseconds>\n", argv[0]);
         exit(1);
     }
-    
+
     int termSeconds = atoi(argv[1]);
     int termNano = atoi(argv[2]);
-    
-    int shm_id = shmget(IPC_PRIVATE, sizeof(int), 0666);
-    int *sysClock = (int *)shmat(shm_id, NULL, 0);
-    
-    int msg_id = msgget(IPC_PRIVATE, 0666);
-    struct msgbuf message;
-    
-    int iterations = 0;
+    int sysClockS = 0, sysClockNano = 0;
+
+    key_t key = ftok("oss.c", 65);
+    int msg_id = msgget(key, 0666);
+
+    printf("WORKER PID:%d -- Started, will run for %d sec %d ns\n", getpid(), termSeconds, termNano);
+
     while (1) {
+        struct msgbuf message;
         msgrcv(msg_id, &message, sizeof(message.data), getpid(), 0);
-        
-        printf("WORKER PID:%d PPID:%d SysClockS:%d TermTimeS:%d\n", getpid(), getppid(), *sysClock, termSeconds);
-        
-        if (*sysClock >= termSeconds) {
-            printf("WORKER PID:%d -- Terminating after %d iterations\n", getpid(), iterations);
-            message.mtype = 1;
-            message.data = 0;
+
+        if (sysClockS > termSeconds || (sysClockS == termSeconds && sysClockNano >= termNano)) {
+            message.data = 0;  // Indicate termination
+            printf("WORKER PID:%d -- Terminating\n", getpid());
             msgsnd(msg_id, &message, sizeof(message.data), 0);
             break;
         }
-        
-        message.mtype = 1;
-        message.data = 1;
+
+        message.data = 1;  // Continue execution
         msgsnd(msg_id, &message, sizeof(message.data), 0);
-        iterations++;
     }
-    
-    shmdt(sysClock);
+
     return 0;
+}
+
 }

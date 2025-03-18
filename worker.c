e807022565c487d33530d8c217ce958ef59381a0
Lbeenc <Lbeenc@gmail.com>
2025-03-18 11:39:05 -0500

    Update worker.c

--- worker.c
+++ worker.c
@@ -1,45 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <sys/types.h>
+#include <sys/shm.h>
 
-struct msgbuf {
+#define CLOCK_KEY 1234
+#define MSG_KEY 5678
+
+struct Clock {
+    int seconds;
+    int nanoseconds;
+};
+
+struct Message {
     long mtype;
     int data;
 };
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        fprintf(stderr, "Usage: worker seconds nanoseconds\n");
+        fprintf(stderr, "Usage: worker <seconds> <nanoseconds>\n");
         exit(1);
     }
 
-    int termSec = atoi(argv[1]);
-    int termNano = atoi(argv[2]);
+    int durationSec = atoi(argv[1]);
+    int durationNano = atoi(argv[2]);
 
-    key_t key = ftok(".", 'm');
-    int msgQueueID = msgget(key, 0666);
-    if (msgQueueID == -1) {
-        perror("Worker: Message queue error");
-        exit(1);
+    int shmid = shmget(CLOCK_KEY, sizeof(struct Clock), 0666);
+    struct Clock *clockPtr = (struct Clock *)shmat(shmid, NULL, 0);
+
+    int msqid = msgget(MSG_KEY, 0666);
+    struct Message msg;
+    int targetSec = clockPtr->seconds + durationSec;
+    int targetNano = clockPtr->nanoseconds + durationNano;
+    
+    if (targetNano >= 1000000000) {
+        targetSec++;
+        targetNano -= 1000000000;
     }
 
-    while (1) {
-        struct msgbuf message;
-        msgrcv(msgQueueID, &message, sizeof(message.data), getpid(), 0);
+    do {
+        msgrcv(msqid, &msg, sizeof(int), getpid(), 0);
 
-        printf("WORKER PID:%d -- Received message at SysClock %d:%d\n", getpid(), termSec, termNano);
+        printf("WORKER PID:%d SysClockS:%d SysClockNano:%d TermTimeS:%d TermTimeNano:%d\n",
+               getpid(), clockPtr->seconds, clockPtr->nanoseconds, targetSec, targetNano);
 
-        if (termSec <= 2) {
-            message.mtype = getpid();
-            message.data = 0;
-            msgsnd(msgQueueID, &message, sizeof(message.data), 0);
-            printf("WORKER PID:%d -- Terminating at SysClock %d:%d\n", getpid(), termSec, termNano);
-            exit(0);
+        if (clockPtr->seconds > targetSec ||
+            (clockPtr->seconds == targetSec && clockPtr->nanoseconds >= targetNano)) {
+            msg.data = 0;
+            msgsnd(msqid, &msg, sizeof(int), 0);
+            printf("WORKER PID:%d -- Terminating.\n", getpid());
+            break;
+        } else {
+            msg.data = 1;
+            msgsnd(msqid, &msg, sizeof(int), 0);
         }
+    } while (1);
 
-        termSec--;
-    }
+    return 0;
 }
+
